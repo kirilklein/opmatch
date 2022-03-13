04d20a54fe99bf616eecd920cb2d72f2944ebc35
Kiril Klein <kleinkiril94@gmail.com>
2022-03-13 10:14:20 +0100

    test new implementation

--- opmatch/tests/create_graph_t.py
+++ opmatch/tests/create_graph_t.py
@@ -3,13 +3,18 @@ ROOT_DIR = os.path.abspath(os.curdir)
 if ROOT_DIR not in sys.path:
     sys.path.append(ROOT_DIR)
 from opmatch.util import create_graph
+from opmatch.util import create_graph_new 
 import create_test_data
+from time import time
 
-X, y, ps = create_test_data.get_test_data(False, 10, 3)
+df = create_test_data.get_test_data(True, 10, 3)
 
 if __name__ == '__main__':
-    out_ = create_graph.create_distance_edge_list_parallel(y, ps, 1)
-    print(out_[0])
-    print(out_[1])
-    print(out_[2])
-    print(len(out_[0]))
+    s0 = time()
+    out0 = create_graph.create_distance_edge_list_parallel(df, 2)
+    print('time for old:', time()-s0)
+    s1 = time()
+    out1 = create_graph_new.create_distance_edge_list_parallel(df, 2)
+    print('time for new:', time()-s1)
+    print('checksum: ', all([o0==o1 for o0, o1 in zip(out0, out1)]))
+    #print(len(out_[0]))
--- opmatch/tests/pd_python.py
+++ opmatch/tests/pd_python.py
@@ -3,28 +3,31 @@ from time import time
 import itertools
 import networkx as nx
 import timeit
-def pairwise_numpy(a, b):
-    return np.abs(a[np.newaxis,:] - b[:, np.newaxis])
-ps_exp = np.arange(1,3)
-ps_nexp = np.arange(4,7)
 
-exp_ids = ps_exp.astype(str)#['1', '2', '3']
-nexp_ids = ps_nexp.astype(str)#['5','6',]
-z_l = [{'capacity':1,}]
+exp_ps = np.arange(1,3)*.1
+nexp_ps = np.arange(4,7)*.1
 
+exp_ids = exp_ps.astype(str)#['1', '2', '3']
+nexp_ids = nexp_ps.astype(str)#['5','6',]
 
 s = time()
-pd_mat = pairwise_numpy(ps_nexp,ps_exp)
-pd = np.ndarray.flatten(pd_mat)
-cap_weight_ls = [{'capacity':1, 'weight':d} for d in pd]
-#print(a)
-#print(np.ndarray.flatten(a)) 
-exp_ids_comb, nexp_ids_comb= zip(*itertools.product(exp_ids, nexp_ids))
-
-
-edge_ls = list(zip(exp_ids_comb,nexp_ids_comb, cap_weight_ls))
-
-print(edge_ls)
+def pairwise_abs_dist(a, b):
+    """Compute absolute distance matrix
+    Input: 1d arrays a and b
+    Returns: len(a)xlen(b) distance matrix
+    """
+    return np.ndarray.astype(np.abs(a[np.newaxis,:] - b[:, np.newaxis])*1e1, int)
+print(pairwise_abs_dist(exp_ps, nexp_ps))
+def create_exp_nexp_edge_ls(exp_ids, nexp_ids, exp_ps, nexp_ps):
+    
+    pdist_mat = pairwise_abs_dist(nexp_ps, exp_ps)
+    pdist = np.ndarray.flatten(pdist_mat)
+    cap_weight_ls = [{'capacity':1, 'weight':dist} for dist in pdist]
+    exp_ids_comb, nexp_ids_comb= zip(*itertools.product(exp_ids, nexp_ids))
+    exp_nexp_edge_ls = list(zip(exp_ids_comb,nexp_ids_comb, cap_weight_ls))
+    return exp_nexp_edge_ls
+exp_nexp_edge_ls = create_exp_nexp_edge_ls(exp_ids, nexp_ids, exp_ps, nexp_ps)
+#print(exp_nexp_edge_ls)
 G = nx.DiGraph()
-G.add_edges_from(edge_ls)
+G.add_edges_from(exp_nexp_edge_ls)
 print(time()-s)
