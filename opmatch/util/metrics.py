d9df91b56ffe226bdd8354cdcd9fac9b2eb7a6b9
Kiril Klein <kleinkiril94@gmail.com>
2022-03-18 11:51:59 +0100

    new metric added

--- opmatch/util/metrics.py
+++ opmatch/util/metrics.py
@@ -14,3 +14,17 @@ def create_ps_dist_matrix(exp_ps:np.ndarray, nexp_ps:np.ndarray,
     assert isinstance(K, int), 'make sure that max_mr and n_controls are integers'
     dist_mat = pairwise_abs_dist(exp_ps, nexp_ps)
     return dist_mat
+
+def standardized_difference_bin(p_t:float,p_c:float):
+    """p_t, p_c: estimated prevalence in trated and control group"""
+    assert (p_t<=1).all() and (0<=p_t).all() and (0<=p_c).all() and (p_c<=1).all(), 'p_t and p_c must be between 0 and 1'
+    num = p_t - p_c
+    den = np.sqrt((p_t*(1-p_t)+p_c*(1-p_c))/2)
+    return np.abs(num/den)
+
+def standardized_difference_con(mu_t:float,mu_c:float, s_t:float, s_c:float):
+    """p_t, p_c: estimated prevalence in treated and control group"""
+    num = mu_t - mu_c
+    den = np.sqrt((s_t**2+s_c**2)/2)
+    return np.abs(num/den)
+
