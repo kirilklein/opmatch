bdf716cd422c127d42779d0623f318a5806c9a70
Kiril Klein <kikl@di.ku.dk>
2022-12-05 08:31:44 +0100

    the propensity score matching does not look correct

--- opmatch/tutorial/example_data.py
+++ opmatch/tutorial/example_data.py
@@ -6,16 +6,25 @@ def convert_scale(clip_a, clip_b, loc, scale):
     a, b = (clip_a - loc) / scale, (clip_b - loc) / scale
     return a, b
     
-def generate(num_patients, num_cases):    
-    age_loc, age_scale = 50, 30
-    bmi_loc, bmi_scale = 20, 10
+def simulate_age(n, age_loc, age_scale):
     age_clip_a, age_clip_b = 0, 100
+    age_a_con, age_b_con = convert_scale(age_clip_a, age_clip_b, age_loc, age_scale)
+    return truncnorm.rvs(a=age_a_con, b=age_b_con, loc=age_loc, scale=age_scale, size=n)
+def simulate_bmi(n, bmi_loc, bmi_scale):
     bmi_clip_a, bmi_clip_b = 0, 50
-    age_a, age_b = convert_scale(age_clip_a, age_clip_b, age_loc, age_scale)
-    bmi_a, bmi_b = convert_scale(bmi_clip_a, bmi_clip_b, bmi_loc, bmi_scale)
-    age = truncnorm.rvs(a=age_a, b=age_b, loc=age_loc, scale=age_scale, size=num_patients)
-    bmi = truncnorm.rvs(a=bmi_a, b=bmi_b, loc=bmi_loc, scale=bmi_scale, size=num_patients)
-    cases_ids = np.random.choice(num_patients, num_cases, replace=False,)
-    case = np.zeros(num_patients)
-    case[cases_ids] = 1
-    return pd.DataFrame({'age':age, 'bmi':bmi, 'case':case})
+    bmi_a_con, bmi_b_con = convert_scale(bmi_clip_a, bmi_clip_b, bmi_loc, bmi_scale)
+    return truncnorm.rvs(a=bmi_a_con, b=bmi_b_con, loc=bmi_loc, scale=bmi_scale, size=n)
+
+def generate(num_patients, num_cases):    
+    age_loc_con, age_loc_case = 40, 60
+    bmi_loc_con, bmi_loc_case = 18, 25
+    age_scale = 20
+    bmi_scale = 10
+    age_controls = simulate_age(num_patients-num_cases, age_loc_con, age_scale)
+    age_cases = simulate_age(num_cases, age_loc_case, age_scale)
+    bmi_controls = simulate_bmi(num_patients-num_cases, bmi_loc_con, bmi_scale)
+    bmi_cases = simulate_bmi(num_cases, bmi_loc_case, bmi_scale)
+    data = {'age': np.concatenate((age_controls, age_cases)), 
+            'bmi': np.concatenate((bmi_controls, bmi_cases)), 
+            'case': np.concatenate((np.zeros(num_patients-num_cases), np.ones(num_cases)))}
+    return pd.DataFrame(data)
--- opmatch/tutorial/vis.py
+++ opmatch/tutorial/vis.py
@@ -29,7 +29,7 @@ def visualize_matched_scatter(df, cc_dics, titles):
     fig.text(0.5, 0.04, 'age', ha='center', va='center')
     fig.text(0.04, 0.5, 'bmi', ha='center', va='center', rotation='vertical')
 
-def plot_ps_matched(ps, case_control_dic,
+def plot_ps_matched(df, case_control_dic,
     title='Optimal PS matching',
     set_title=True,
     title_fs=18,
@@ -38,7 +38,7 @@ def plot_ps_matched(ps, case_control_dic,
     ylabel='Case Number',
     ylabel_fs=18,
     case_label='case',
-    control_label='control',
+    control_label='matched controls',
     legend=True,
     legend_fs=18,
     legend_pos=0,
@@ -58,7 +58,8 @@ def plot_ps_matched(ps, case_control_dic,
     
     fig, ax = plt.subplots(figsize=figsize)
     case_ids = list(case_control_dic.keys())
-
+    ps = df.ps
+    ps_control = df[df.case==0].ps
     for i, case in enumerate(case_ids):
         if i==0:
             case_label = case_label
@@ -68,12 +69,14 @@ def plot_ps_matched(ps, case_control_dic,
         if isinstance(color, type(None)):
             color0 = list(plt.rcParams['axes.prop_cycle'])[0]['color']
             color1 = list(plt.rcParams['axes.prop_cycle'])[1]['color']
-        ax.scatter(ps[case], i, marker=markerstyle_case, color=color0, 
+        ax.scatter(ps[case], i+1, marker=markerstyle_case, color=color0, 
                 label=case_label, s=markersize_case)
         control_ps = ps[case_control_dic[case]]
         group_arr = np.ones(len(control_ps))*i
         ax.scatter(control_ps, group_arr, marker=markerstyle_control, color=color1, 
                 label=control_label, s=markersize_control)
+    ax.scatter(ps_control, np.zeros(len(ps_control)), label = 'all controls', color='gray', marker='.', s=markersize_control)
+    
     if legend:
         ax.legend(fontsize=legend_fs, loc=legend_pos, facecolor=legend_facecolor)
     if set_title:
@@ -89,4 +92,36 @@ def plot_ps_matched(ps, case_control_dic,
     if save:
         fig.savefig(figname, dpi=dpi)
     
+def standardized_difference_plot(df, cc_dic, var_cols=['age', 'bmi']):
+    # plot standardized difference between cases and controls
+    fig, ax = plt.subplots(figsize=(4,3))
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    cases = df[df.case==1]
+    controls = df[df.case==0]
+    matched = []
+    for case, controls_ls in cc_dic.items():
+        matched.extend(controls_ls)
+    matched_controls = df.loc[matched]
+    # before matching
+    for i, var in enumerate(var_cols):
+        if i==0:
+            label='before matching'
+            matched_lbl = 'matched'
+        else:
+            label=None
+            matched_lbl = None
+        sd = np.abs((cases[var].mean() - controls[var].mean())/np.sqrt((cases[var].std()**2 + controls[var].std()**2)/2))
+        matched_sd = np.abs((cases[var].mean() - matched_controls[var].mean())/np.sqrt((cases[var].std()**2 + matched_controls[var].std()**2)/2))
+        ax.scatter(sd, i, color=colors[0], label=label, marker='o', s=40, facecolors='none')
+        ax.scatter(matched_sd, i, color=colors[0], label=matched_lbl, marker='.', s=80)
+    # set y ticks to var names
+    ax.set_yticks(range(len(var_cols)))
+    ax.set_yticklabels(var_cols)
+    ax.set_xlabel('Absolute standardized difference')
+    ax.legend()
+    # get all matched controls
+    
+    
+    # after matching
+    # now compute standardized difference for each case-control pair
     
